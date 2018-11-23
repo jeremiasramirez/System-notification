f6c36e816c99948ced52c481fc1acfad03ccc4ef
Jeremias ramirez <jereramirez499@gmail.com>
2018-11-23 00:15:56 -0400

    Deleting item

--- pug/pug/javascript/index.js
+++ pug/pug/javascript/index.js
@@ -2,7 +2,8 @@
 		  
 let notificationPanel = document.getElementById("notification__panel-js");
 let closePanel = document.getElementById("quit-panel-js");
- 
+
+
 
 if(notificationPanel && closePanel){
 
@@ -12,3 +13,39 @@ if(notificationPanel && closePanel){
 	},false)
 
 }
+
+let titleToday = document.getElementById("title-today-js");
+
+let notiToday = document.getElementById("notification__today-js");
+	let notiTodayItem = Array.from(notiToday.querySelectorAll('div'))
+
+	for(let i=0;i<notiTodayItem.length;i++){
+		notiTodayItem[i].addEventListener("click",function(e){
+			e.target.remove()
+
+		if(notiToday.children[1] === undefined){
+			titleToday.textContent= 'No hay notificaciones'
+		}
+		else{
+			true
+		}
+		},false);
+
+	}
+	console.log( )
+let notiTomorrow = document.getElementById("notification__tomorrow-js");
+let notiTomorrowTitle = document.getElementById("notification__tomorrow-title");
+	let notiTomorrowItem = Array.from(notiTomorrow.querySelectorAll('div'))
+
+		for(let i=0;i<notiTomorrowItem.length;i++){
+		notiTomorrowItem[i].addEventListener("click",function(e){
+			e.target.remove()
+			if(notiTomorrow.children[1] == undefined){
+				notiTomorrowTitle.textContent = 'No hay notificaciones'
+			}
+		},false);
+	}
+ 
+
+
+
